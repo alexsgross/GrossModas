feef8818d54f91bcff831078480555b71ab26576
Alex Gross <alexsgross@hotmail.com>
2022-03-09 09:01:18 -0300

    node

---
+++ backend/data.js
@@ -0,0 +1,57 @@
+const data = {
+    products: [
+        {
+            name: 'Camisa Social',
+            slug: 'Camisa-Social',
+            categoty: 'camisas',
+            image: '/images/p1.jpg',
+            price: 'R$ 100,00',
+            countInStock: 10,
+            brand: 'Pierre Cardin',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Camisa social 100% algodão',
+
+        },
+        {
+            name: 'Camisa Social em poliesteer e algodão',
+            slug: 'camisa-social-poliester-algodao',
+            categoty: 'camisas',
+            image: '/images/p2.jpg',
+            price: 'R$ 89,99',
+            countInStock: 40,
+            brand: 'Pierre Frabrege',
+            rating: 2.5,
+            numReviews: 6,
+            description: 'Camisa social em poliester e algodão',
+
+        },{
+            name: 'Calça Social Tergal Linho',
+            slug: 'calca-social-tergal-linho',
+            categoty: 'calcas',
+            image: '/images/p3.jpg',
+            price: 'R$ 60,00',
+            countInStock: 20,
+            brand: 'Lupo',
+            rating: 1.5,
+            numReviews: 9,
+            description: 'Camisa social de botões',
+
+        },
+        {
+            name: 'Calça Social Sport Fino 100% algodão',
+            slug: 'calca-social-100-algodao',
+            categoty: 'calcas',
+            image: '/images/p4.jpg',
+            price: 'R$ 30,00',
+            countInStock: 60,
+            brand: 'Bruno Mainardi',
+            rating: 4.5,
+            numReviews: 8,
+            description: 'Camisa polo 100% algodão',
+
+        },       
+    ],
+};
+
+export default data;
