a1cc67ce8ba3778209844be3110e14f80512cf91
Alex Gross <alexsgross@hotmail.com>
2022-03-12 11:42:26 -0300

    react bootstrap mais footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import {LinkContainer} from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Gross Modas</Link> 
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Gross Modas</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>  
+          
         </header>
         <main>
-          <Routes>
-            <Route path="/products/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>          
+          <Container>
+            <Routes>
+              <Route path="/products/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>  
+          </Container>        
         </main>
+        <footer>
+          <div className="text-center"> Alex Gross - Todos os direitor reservados </div>
+          </footer>
       </div>
     </BrowserRouter>
   );
